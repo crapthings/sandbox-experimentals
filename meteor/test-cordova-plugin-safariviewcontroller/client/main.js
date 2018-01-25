b64b7312ba4019502e7bd098402af91dfbcd95b8
crapthings <crapthings@gmail.com>
2018-01-25 15:54:19 +0800

    iap alternative can't build

---
+++ meteor/test-cordova-plugin-safariviewcontroller/client/main.js
@@ -0,0 +1,28 @@
+Template.btn.events({
+
+  'click button'(event, instance) {
+    cordova.plugins.barcodeScanner.scan(
+      result => {
+        alert(JSON.stringify(result, null, 2))
+      },
+
+      error => {
+        alert(JSON.stringify(error, null, 2))
+      },
+
+      {
+        showFlipCameraButton: true, // iOS and Android
+        showTorchButton: true, // iOS and Android
+        torchOn: false, // Android, launch with the torch switched on (if available)
+        saveHistory: true, // Android, save scan history (default false)
+        prompt: 'Place a barcode inside the scan area', // Android
+        resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+        formats: 'QR_CODE,PDF_417', // default: all but PDF_417 and RSS_EXPANDED
+        orientation: 'portrait', // Android only (portrait|landscape), default unset so it rotates with the device
+        disableAnimations: true, // iOS
+        disableSuccessBeep: false // iOS and Android
+      }
+   )
+  },
+
+})
